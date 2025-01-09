3fc68885154a29c85c34b68b3b316f8d8680cbb8
stormValet <nirmaldh759@gmail.com>
2025-01-09 20:46:31 +0530

    Test Commit

---
+++ First 1:Options API/src/App.vue
@@ -0,0 +1,46 @@
+<script>
+  export default {
+    data(){
+      return {
+        name: 'John Doe',
+        status: 'pending',
+        tasks: ['Task One', 'Task Two', 'Task Three'],
+        link: 'https://www.google.com'
+      }
+    },
+    methods:{
+      toggleStatus(){
+        if(this.status === 'active') {
+          this.status = 'pending';
+        }else if(this.status === 'pending'){
+          this.status = 'inactive';
+        }else{
+          this.status = 'active';
+        }
+      }
+    }
+  }
+</script>
+
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <h3> Tasks : </h3>
+  <ul>
+    <li v-for="task in tasks" :keys="task">{{ task }}</li>
+  </ul>
+  <!-- <a v-bind:href="link">Click me to redirect</a> -->
+  <a  :href="link">Click me to redirect</a>
+
+  <button v-on:click="toggleStatus">click to toggle</button>
+</template>
+
+<style scope>
+  h1{
+    color : red;
+  }
+</style>
